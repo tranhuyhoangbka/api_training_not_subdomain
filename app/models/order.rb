00f3f399bb1cc151c786df4664c914bd102107c5
tranhuyhoangbka <hoangth92.nd@gmail.com>
2016-04-18 17:14:17 +0700

    Add validator for order with not enough products on stock

--- app/models/order.rb
+++ app/models/order.rb
@@ -5,6 +5,7 @@ class Order < ActiveRecord::Base
 
   validates :user_id, presence: true
   validates :total, presence: true, numericality: {greater_than_or_equal_to: 0}
+  validates_with EnoughProductsValidator
 
   before_validation :set_total!
 
