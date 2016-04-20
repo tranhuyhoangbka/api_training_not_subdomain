20c673a4fc71944e334bbc3a7288e8cb54e85ab4
tranhuyhoangbka <hoangth92.nd@gmail.com>
2016-04-20 10:00:46 +0700

    Adds pagination for products index action to optimize response

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -4,7 +4,13 @@ class Api::V1::ProductsController < ApplicationController
   respond_to :json
 
   def index
-    respond_with Product.all
+    products = Product.all.page(params[:page]).per params[:per_page]
+    render json: products, meta:
+    {pagination:
+      {per_page: params[:per_page],
+      total_pages: products.total_pages,
+      total_objects: products.total_count}
+    }
   end
 
   def show
